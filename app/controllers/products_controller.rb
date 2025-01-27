2a1fd69ab959c96f27774a67fbc8c446f6af0765
bsdev <serhiinovak02@gmail.com>
2025-01-27 11:29:53 -0800

    Update product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: %i[show edit update]
+  before_action :set_product, only: %i[show edit update destroy]
 
   def index
     @products = Product.all
@@ -25,11 +25,16 @@ class ProductsController < ApplicationController
   end
 
   def update
-    if @proudct.update(product_params)
+    if @product.update(product_params)
       redirect_to @product
     else
       render :edit, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to products_path
   end
 
   private
